c29261bbfafde891e92d6932808435a460b4544f
Owen Connor <owenconnor@protonmail.com>
2023-03-07 15:55:11 +0000

    added learning_paths association, renamed course_enrollments

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :author_profile
-  has_many :enrollments
-  has_many :student_profiles, through: :enrollments
+  has_many :course_enrollments
+  has_many :student_profiles, through: :course_enrollments
+  has_and_belongs_to_many :learning_paths
 end
