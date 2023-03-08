0e8209eb83f4d2dcefc3c2e04d1dabf4e9824904
Owen Connor <owenconnor@protonmail.com>
2023-03-08 09:51:11 +0000

    updated associations for student_profiles

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -1,3 +1,7 @@
 class StudentProfile < ApplicationRecord
   belongs_to :user
+  has_many :course_enrollments
+  has_many :courses, through: :course_enrollments
+  has_many :learning_path_enrollments
+  has_many :learning_paths, through: :learning_path_enrollments
 end
