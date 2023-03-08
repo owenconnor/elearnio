00c974ca7547228136320618b8dcee50fc642fee
Owen Connor <owenconnor@protonmail.com>
2023-03-08 11:02:40 +0000

    padded out courses_controller WIP

--- app/controllers/v1/courses_controller.rb
+++ app/controllers/v1/courses_controller.rb
@@ -1,42 +1,46 @@
 class V1::CoursesController < ApplicationController
   def index
     @courses = Course.all
-
     render json: @courses, status: :ok
   end
 
   def show
     @course = Course.find(params[:id])
-
     render json: @course, status: :ok
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { params: params, error: e.to_s }, status: :not_found
   end
 
   def create
-    @course = Course.new(course_params)
-    @course.save
+    @course = Course.create(course_params)
+    Rails.logger.info "Errors: #{@course.errors.full_messages} -------"
     render json: @course, status: :created
   end
 
   def update
     @course = Course.find(params[:id])
     if @course.update(course_params)
-      render json: @course, status: :created
+      render json: @course, status: :ok
     else
-      render json: @course.errors, status: :unprocessable_entity
+      render json: { errors: @course.errors.full_messages, params: course_params }, status: :unprocessable_entity
     end
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { params: course_params, error: e.to_s }, status: :not_found
   end
 
   def destroy
     @course = Course.find(params[:id])
     if @course.destroy
       head(:ok)
-    else
-      head(:unprocessable_entity)
+    # else
+    #   head(:unprocessable_entity)
     end
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { params: params, error: e.to_s }, status: :not_found
   end
 
   private
     def course_params
-      params.require(:course).permit(:title, :description, :author_profile_id, :learning_paths)
+      params.require(:course).permit(:title, :author_profile_id)
     end
 end
