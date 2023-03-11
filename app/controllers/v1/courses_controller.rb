7e6f50227743e606315855c242b5cfb2636fc87b
Owen Connor <owenconnor@protonmail.com>
2023-03-11 13:06:37 +0000

    basic yard documentation

--- app/controllers/v1/courses_controller.rb
+++ app/controllers/v1/courses_controller.rb
@@ -1,10 +1,19 @@
 class V1::CoursesController < ApplicationController
-  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
+  # The following is a custom error handler for ActiveRecord::RecordNotFound
+  # It will return a 404 status code and a JSON response with the error message
+  # and the request parameters
   def record_not_found(e)
     render json: { params: params, error: e.to_s }, status: :not_found
   end
 
+  # The index action will return a list of all courses
+  # It will include the author, students, and learning paths
+  # It will return a 200 status code and a JSON response with the list of courses
+  # If there are no courses, it will return an empty array
+  # If there is an error, it will return a 404 status code and a JSON response with the error message
+  # and the request parameters
+  # GET /v1/courses
   def index
     courses = Course.includes(:author_profile, :student_profiles, :learning_paths).all
     render json: courses, each_serializer: CourseIndexSerializer, status: :ok
@@ -12,6 +21,12 @@ class V1::CoursesController < ApplicationController
     record_not_found(e)
   end
 
+  # The show action will return a single course
+  # It will include the author, students, and learning paths
+  # It will return a 200 status code and a JSON response with the course
+  # If the course is not found, it will return a 404 status code and a JSON response with the error message
+  # and the request parameters
+  # GET /v1/courses/:id
   def show
     course = Course.find(params[:id])
     if course.persisted?
@@ -23,6 +38,13 @@ class V1::CoursesController < ApplicationController
     record_not_found(e)
   end
 
+  # The create action will create a new course
+  # It will call the CourseShowSerializer to return the newly created course
+  # It will return a 201 status code and a JSON response with the newly created course
+  # If the course is not created, it will return a 422 status code and a JSON response with the error message
+  # and the request parameters
+  # POST /v1/courses
+  #
   def create
     course = Course.create(course_params)
     if course.persisted?
@@ -32,6 +54,14 @@ class V1::CoursesController < ApplicationController
     end
   end
 
+  # The update action will update an existing course
+  # It will call the CourseShowSerializer to return the updated course
+  # It will return a 200 status code and a JSON response with the updated course
+  # If the course is not updated, it will return a 422 status code and a JSON response with the error message
+  # and the request parameters
+  # if the course is not found, it will return a 404 status code and a JSON response with the error message
+  # and the request parameters
+  # PUT /v1/courses/:id
   def update
     course = Course.find(params[:id])
     if course.update(course_params)
