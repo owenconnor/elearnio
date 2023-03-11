ea41f6064af724a7b196e52395ed2859258bd89e
Owen Connor <owenconnor@protonmail.com>
2023-03-11 13:02:25 +0000

    added methods to view courses

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -4,4 +4,12 @@ class StudentProfile < ApplicationRecord
   has_many :courses, through: :course_enrollments
   has_many :learning_path_enrollments
   has_many :learning_paths, through: :learning_path_enrollments
+
+  def enrolled_courses
+    course_enrollments.where(status: "enrolled").map(&:course)
+  end
+
+  def completed_courses
+    course_enrollments.where(status: "completed").map(&:course)
+  end
 end
