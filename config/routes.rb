4d66717a5d50deaf9c6f080901a2008065c05e42
Owen Connor <owenconnor@protonmail.com>
2023-03-12 21:45:35 +0000

    removed versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    resources :users
-    resources :student_profiles
-    resources :learning_paths do
-      post 'subscribe', on: :member
-    end
-    resources :author_profiles
-    resources :courses do
-      post 'enroll', on: :member
-      post 'complete', on: :member
-    end
+  resources :users
+  resources :student_profiles
+  resources :learning_paths do
+    post 'subscribe', on: :member
+  end
+  resources :author_profiles
+  resources :courses do
+    post 'enroll', on: :member
+    post 'complete', on: :member
   end
 end
