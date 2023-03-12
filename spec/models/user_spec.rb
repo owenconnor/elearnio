ec2e240ef5ad0c64e4fd49bd4802fcfde4cc815f
Owen Connor <owenconnor@protonmail.com>
2023-03-12 14:49:06 +0000

    added some api documentation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_one(:author_profile) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  describe 'instance methods' do
+
+  end
+
+  describe 'class methods' do
+
+  end
+
+  describe 'callbacks' do
+
+  end
+
+  describe 'scopes' do
+
+  end
+
+  describe 'custom validations' do
+
+  end
 end
