411288afd5fd1335bc40e2c43ffa1157005515c1
Owen Connor <owenconnor@protonmail.com>
2023-03-12 21:47:11 +0000

    basic docs

--- app/models/author_profile.rb
+++ app/models/author_profile.rb
@@ -1,4 +1,13 @@
+# Represents an author profile for a user
 class AuthorProfile < ApplicationRecord
+  # @!attribute [r] id
+  #  @return [Integer] the unique identifier for the author profile
+  # @!attribute [rw] user_id
+  # @!attribute [rw] courses
+  # @return [Array<Course>] the courses authored by the user
+  # @!attribute [rw] user
+  # @return [User] the user associated with the author profile
+
   belongs_to :user
   has_many :courses
 
