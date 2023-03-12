a454b87a61abb36e2a3c9a6d93cc1f39b68aee21
Owen Connor <owenconnor@protonmail.com>
2023-03-12 14:56:24 +0000

    basic validations

--- app/models/course_enrollment.rb
+++ app/models/course_enrollment.rb
@@ -1,5 +1,18 @@
 class CourseEnrollment < ApplicationRecord
   belongs_to :course
   belongs_to :student_profile
+
+  validates :course_id, presence: true
+  validates :student_profile_id, presence: true
+  validates :course_id, uniqueness: { scope: :student_profile_id }
+  validates :status, inclusion: { in: %w(enrolled completed) }
+
+  validate :student_profile_is_not_already_enrolled_in_course, on: :create
+
+  def student_profile_is_not_already_enrolled_in_course
+    if CourseEnrollment.where(course: course, student_profile: student_profile).any?
+      errors.add(:student_profile, "is already enrolled in this course")
+    end
+  end
 end
 
