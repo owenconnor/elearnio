6d629c759cd97a298465e9e659cbb8025b87d8a2
Owen Connor <owenconnor@protonmail.com>
2023-03-12 22:27:00 +0000

    fix for n+1 in student_profile.rb

--- app/models/student_profile.rb
+++ app/models/student_profile.rb
@@ -6,10 +6,10 @@ class StudentProfile < ApplicationRecord
   has_many :learning_paths, through: :learning_path_enrollments
 
   def enrolled_courses
-    course_enrollments.where(status: "enrolled").map(&:course)
+    course_enrollments.includes(:course).where(status: "enrolled").map(&:course)
   end
 
   def completed_courses
-    course_enrollments.where(status: "completed").map(&:course)
+    course_enrollments.includes(:course).where(status: "completed").map(&:course)
   end
 end
