186b3dfe9040bde2474c14f86d9f954e74e7f036
Owen Connor <owenconnor@protonmail.com>
2023-03-12 14:52:47 +0000

    updated factory traits

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,41 +3,24 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     email { Faker::Internet.email }
-
-    trait :with_student_profile do
-      after(:create) do |user|
-        user.create_student_profile
-      end
-    end
-
-    trait :with_author_profile do
-      after(:create) do |user|
-        user.create_author_profile
-      end
-    end
-
-    trait :with_student_and_author_profiles do
-      after(:create) do |user|
-        user.create_student_profile
-        user.create_author_profile
-      end
-    end
   end
 
   factory :course do
     title { Faker::Educator.course_name }
-    author_profile_id { FactoryBot.create(:user, :with_author_profile).author_profile.id }
+    author_profile_id { FactoryBot.create(:user).author_profile.id }
 
     trait :with_students do
       after(:create) do |course|
-        course.student_profiles << FactoryBot.create_list(:user, 5, :with_student_profile).map(&:student_profile)
-      end
-
-    trait :with_learning_paths do
-      after(:create) do |course|
-        course.learning_paths << FactoryBot.create_list(:learning_path, 5).map(&:learning_path)
+        5.times do
+          course.course_enrollments.create(student_profile_id: FactoryBot.create(:user).student_profile.id, status: "enrolled")
+        end
       end
     end
+
+    trait :with_learning_path do
+      after(:create) do |course|
+        course.learning_paths << FactoryBot.create_list(:learning_path, 5)
+      end
     end
   end
 
@@ -49,10 +32,9 @@ FactoryBot.define do
         learning_path.courses << FactoryBot.create_list(:course, 5)
       end
     end
-
     trait :with_students do
       after(:create) do |learning_path|
-        learning_path.student_profiles << FactoryBot.create_list(:user, 5, :with_student_profile).map(&:student_profile)
+        learning_path.learning_path_enrollment.create(student_profile_id: FactoryBot.create(:user).student_profile.id, status: "subscribed")
       end
     end
   end
