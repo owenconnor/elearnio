148291f10cbb03f487a9e0c1229698b8bd0f07ce
Owen Connor <owenconnor@protonmail.com>
2023-03-12 14:50:34 +0000

    added callbacks for profile creation and scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  after_create :create_student_profile
+  after_create :create_author_profile
+
+  scope :subscribed_learning_paths, -> (user) { user.student_profile.learning_path_enrollments.where(status: "subscribed").map(&:learning_path) }
+  scope :completed_learning_paths, -> (user) { user.student_profile.learning_path_enrollments.where(status: "completed").map(&:learning_path) }
 end
