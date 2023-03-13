478c22724d2723cce2cb4e1d57fd9c0de123ac05
Owen Connor <owenconnor@protonmail.com>
2023-03-13 01:42:02 +0000

    fix for spec

--- spec/models/author_profile_spec.rb
+++ spec/models/author_profile_spec.rb
@@ -5,10 +5,11 @@ RSpec.describe AuthorProfile, type: :model do
     describe '#transfer_courses' do
       let(:author) { create(:user) }
       let(:course) { create(:course, author_profile: author.author_profile) }
-      let(:new_author) { create(:user) }
+      let(:new_author_profile) { create(:user) }
 
       it 'transfers courses to another author profile' do
-        expect { author.author_profile.destroy }.to change { course.reload.author_profile }.from(author.author_profile).to(new_author.author_profile)
+        # new_author_profile = new_author_profiles.first
+        expect { author.author_profile.destroy }.to change { course.reload.author_profile_id }.from(author.author_profile.id).to(new_author_profile.author_profile_id)
       end
     end
   end
