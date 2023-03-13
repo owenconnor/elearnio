bc474ef496856fb03ce4c95d2788bb0135dbcea2
Owen Connor <owenconnor@protonmail.com>
2023-03-13 01:43:03 +0000

    documentation

--- app/controllers/learning_paths_controller.rb
+++ app/controllers/learning_paths_controller.rb
@@ -1,14 +1,28 @@
+# This controller is responsible for handling the requests for the LearningPath model
 class LearningPathsController < ApplicationController
 
   def record_not_found(e)
     # render json: { error: 'Course not found' }, status: :not_found
     render json: { params: params, error: e.to_s }, status: :not_found
   end
+  #@!method index
+  # The index action will return all the learning paths
+  # It will include the courses and students
+  # It will return a 200 status code and a JSON response with the learning paths
+  # GET /learning_paths
   def index
     learning_paths = LearningPath.includes(:courses, :student_profiles).all
     render json: learning_paths, each_serializer: LearningPathIndexSerializer, status: :ok
   end
 
+  #@!method show
+  # The show action will return a single learning path
+  # It will include the courses, students and learning paths
+  # It will return a 200 status code and a JSON response with learning path object, course objects and student profile objects
+  # If the learning path is not found, it will return a 404 status code and a JSON response with the error message
+  # and the request parameters
+  # @param [Integer] id The id of the learning path to be returned
+  # GET /learning_paths/:id
   def show
     learning_path = LearningPath.find(params[:id])
     render json: learning_path, serializer: LearningPathShowSerializer, status: :ok
@@ -16,10 +30,17 @@ class LearningPathsController < ApplicationController
     record_not_found(e)
   end
 
+  #@!method create
+  # The create action will create a new learning path
+  # It will call the LearningPathShowSerializer to return the newly created learning path
+  # It will return a 201 status code and a JSON response with the newly created learning path
+  # If the learning path is not created, it will return a 422 status code and a JSON response with the error message
+  # and the request parameters
+  # @param [String] title The name of the learning path
   def create
     learning_path = LearningPath.create(learning_path_params)
     if learning_path.persisted?
-      render json: learning_path, status: :created
+      render json: learning_path, serializer: LearningPathShowSerializer, status: :created
     else
       render json: { errors: learning_path.errors.full_messages, params: learning_path_params }, status: :unprocessable_entity
     end
@@ -27,6 +48,13 @@ class LearningPathsController < ApplicationController
     record_not_found(e)
   end
 
+  #@!method update
+  # The update action will update an existing learning path
+  # It will call the LearningPathShowSerializer to return the updated learning path
+  # It will return a 200 status code and a JSON response with the updated learning path
+  # If the learning path is not updated, it will return a 422 status code and a JSON response with the error message
+  # and the request parameters
+  # @param [Integer] id The id of the learning path to be updated
   def update
     learning_path = LearningPath.find(params[:id])
     if learning_path.update(learning_path_params)
@@ -38,6 +66,13 @@ class LearningPathsController < ApplicationController
     record_not_found(e)
   end
 
+  #@!method destroy
+  # The destroy action will delete an existing learning path
+  # It will return a 200 status code and a JSON response with the message
+  # If the learning path is not deleted, it will return a 422 status code and a JSON response with the error message
+  # and the request parameters
+  # @param [Integer] id The id of the learning path to be deleted
+  # DELETE /learning_paths/:id
   def destroy
     learning_path = LearningPath.find(params[:id])
     learning_path.destroy
@@ -48,6 +83,14 @@ class LearningPathsController < ApplicationController
     render json: { error: e.to_s  }, status: :unprocessable_entity
   end
 
+  #@!method subscribe
+  # The subscribe action will subscribe a student to a learning path by creating a new [LearningPathEnrollment]
+  # It will call the LearningPathShowSerializer to return the updated learning path
+  # It will return a 200 status code and a JSON response with the updated learning path
+  # If the learning path is not updated, it will return a 422 status code and a JSON response with the error message
+  # and the request parameters
+  # POST /learning_paths/:id/subscribe/:student_profile_id
+  # @param [Integer] id The id of the learning path to be subscribed to
   def subscribe
     learning_path = LearningPath.find(params[:id])
     student_profile = StudentProfile.find(params[:student_profile_id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
-  def index
+  #@!method index
+  # The index action will return a list of all users
+  # It will include the student profile and author profile
+  # It will return a 200 status code and a JSON response with the list of users
+  # If there are no users, it will return an empty array
+   def index
     users = User.includes(:student_profile, :author_profile).all
     render json: users, each_serializer: UserIndexSerializer, status: :ok
   end
 
+  #@!method show
+  # The show action will return a single user
+  # It will include the student profile and author profile, enrolled courses,completed courses, and subscribed learning paths
+  # It will return a 200 status code and a JSON response with the user
+  # If the user is not found, it will return a 404 status code and a JSON response with the error message
+  # and the request parameters
+  # @param [Integer] id The id of the user.
+  # GET /users/:id
   def show
     user = User.find(params[:id])
     render json: user, serializer: UserShowSerializer, status: :ok
@@ -11,39 +24,41 @@ class UsersController < ApplicationController
     render json: { params: params, error: e.to_s }, status: :not_found
   end
 
-  # @api {post} /users
-  # Create a new user.
-  # @example Creating a new user
-  #   POST /users
-  #   { user: {
-  #     first_name: "John",
-  #     last_name: "Doe",
-  #     email: "johndoe@example.com"
-  #   }
-  #   =>
-  #   {
-  #     "id": 1,
-  #     "name": "John Doe",
-  #     "email": "johndoe@example.com"
-  #   }
-  # This is a method is used to create a new user.
+  # @!method create
+  # The create action will create a new user
+  # It will call the UserShowSerializer to return the newly created user
+  # It will return a 201 status code and a JSON response with the newly created user
+  # If the user is not created, it will return a 422 status code and a JSON response with the error message
+  # and the request parameters
+  # POST /users
+  # @param [String] email The email of the user.
   # @param [String] first_name The first name of the user.
   # @param [String] last_name The last name of the user.
-  # @param [String] email The email of the user.
   def create
     @user = User.create(user_params)
     if @user.persisted?
-      render json: @user, status: :created
+      render json: @user, serializer: UserShowSerializer, status: :created
     else
       render json: { errors: @user.errors.full_messages, params: user_params }, status: :unprocessable_entity
     end
   end
 
+  # @!method update
+  # The update action will update an existing user
+  # It will call the UserShowSerializer to return the updated user
+  # It will return a 200 status code and a JSON response with the updated user
+  # if the user is not found, it will return a 404 status code and a JSON response with the error message
+  # and the request parameters
+  # PUT /users/:id
+  # @param [String] email The email of the user.
+  # @param [String] first_name The first name of the user.
+  # @param [String] last_name The last name of the user.
+  # @param [Integer] id The id of the user.
   def update
     #todo: this isn't working correctly
     @user = User.find(user_params[:id])
     if @user.update(user_params)
-      render json: @user, status: :ok
+      render json: @user, serializer: UserShowSerializer, status: :ok
     else
       render json: { errors: @user.errors.full_messages, params: user_params }, status: :unprocessable_entity
     end
@@ -52,10 +67,18 @@ class UsersController < ApplicationController
   rescue
   end
 
+  # @!method destroy
+  # The destroy action will delete an existing user
+  # It will return a 200 status code and a JSON response with the message
+  # if the user is not found, it will return a 404 status code and a JSON response with the error message
+  # and the request parameters
+  # DELETE /users/:id
+  # @param [Integer] id The id of the user.
   def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-    render json: { message: 'User deleted successfully' }, status: :ok
+    user = User.find(params[:id])
+    if user.destroy
+      render json: { message: 'User deleted successfully' }, status: :ok
+    end
   rescue ActiveRecord::RecordNotFound => e
     render json: { error: e.to_s }, status: :not_found
   rescue ActiveRecord::RecordNotDestroyed => e
